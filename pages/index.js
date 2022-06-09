e46b4e4f22dd82285b60be20a8133920be0f0dd8
Brandon Hills <eacrataes@gmail.com>
2022-06-08 22:10:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -8,7 +7,10 @@ export default function Home() {
 			<Head>
 				<title>Bran</title>
 				<meta name='description' content='Bless the Maker and His Water' />
-				<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐳</text></svg>" />
+				<link
+					rel='icon'
+					href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐳</text></svg>'
+				/>
 			</Head>
 
 			<main className={styles.main}>
