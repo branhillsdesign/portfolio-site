44fd05b74ff61ffa9c89ef48e48fb6409e359870
Brandon Hills <eacrataes@gmail.com>
2022-08-02 14:17:55 -0500

    analytics fix hopefully

---
+++ pages/about/golf.jsx
@@ -0,0 +1,25 @@
+import { PageWrapper } from '../components/layout/PageWrapper.jsx';
+import { PageHeading } from '../components/layout/PageHeading.jsx';
+import { Highlighter } from '../components/fonts/Highlighters.jsx';
+import { Reel } from '../components/layout/Reel.jsx';
+import { Footer } from '../components/layout/Footer.jsx';
+import {
+	DesktopMainNav,
+	DesktopSideNav,
+} from '../components/layout/Navigation.jsx';
+
+export default function Golf() {
+	return (
+		<>
+			<PageWrapper>
+				<DesktopMainNav />
+				<PageHeading>
+					Under<Highlighter>Construction</Highlighter>
+				</PageHeading>
+				<DesktopSideNav />
+				<Reel></Reel>
+				<Footer />
+			</PageWrapper>
+		</>
+	);
+}
